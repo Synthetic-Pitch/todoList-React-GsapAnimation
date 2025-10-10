766d9e8ede0794ea20be2bb0a94ca9dbc4379792
reymark <genuinepower123@gmail.com>
2025-10-10 14:23:14 +0800

    main

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,40 +1,61 @@
 import { UseTodo } from "../customHook/todoHook";
+import type { ItemType } from '../customHook/todoHook';
+import { Tooltip as ReactTooltip} from 'react-tooltip';
 
 const TodoForm = () => {
-    const {AddItem,item,setItem} = UseTodo();
-    
+    const {AddItem,item,setItem,DeleteItem,setDeleteList,toggleToolTp,emptyInput} = UseTodo();
+
     return (
         <div className="w-full max-w-[1200px] flex flex-col items-center">
             <textarea rows={4} className="w-full tablet:w-[80%] py-4 px-2 outline-1 rounded-xl outline-gray-700 text-xl"></textarea>
             
             <section className="w-full px-4 py-6">
                 <aside className="flex gap-4 select-none">
-                    <button className="cursor-pointer px-2 bg-[red] rounded-xl" onClick={AddItem}>+addItem</button>
-                    <button className="cursor-pointer px-2 bg-[red] rounded-xl">delete</button>
-                    <button className="cursor-pointer px-2 bg-[red] rounded-xl">date</button>
+                    <button 
+                        className="cursor-pointer px-2 bg-[red] rounded-xl" onClick={AddItem}>+addItem</button>
+                    <button
+                        className="cursor-pointer px-2 bg-[red] rounded-xl"
+                        onClick={DeleteItem}
+                    >delete</button>
+                    <button 
+                        className="cursor-pointer px-2 bg-[red] rounded-xl">date</button>
                 </aside>
             </section>
-
+            
+            {/* This is Item's map*/}
             <main className="w-full pl-4">{
-                item.map((_,index:number)=>(
-                    <div key={index} className="flex gap-2 mt-2">
-                        <input type="checkbox" className="h-6 w-6" />
+                item.map((i:ItemType,index:number)=>(
+                    <div key={i.id} className="flex gap-2 mt-2">
+                        <input
+                            type="checkbox"
+                            className="h-6 w-6"
+                            onChange={(e)=>{
+                                if(e.target.checked){
+                                    setDeleteList(prev=>[...prev,index]);
+                                }else{
+                                    setDeleteList(prev=>prev.filter((d)=> d !== index))
+                                }
+                            }}
+                         />
                         <input 
                             type="text" 
-                            className="bg-[white] outline-0 px-2 h-6" 
-                            value={item[index]}
+                            data-tooltip-id={`${emptyInput === index ? 'my-tooltip':''}`}
+                            data-tooltip-content="please add item"
+                            className="bg-[white] outline-0 px-2 h-6"
+                            value={i.text}
                             onChange={(e)=>{
-                                const val = e.target.value;
-                                 setItem(prev => {
-                                    const newItems = [...prev]; // copy the array
-                                    newItems[index] = val;      // update specific index
-                                    return newItems;            // return new array
+                                setItem(prev=>{
+                                    const arr = [...prev];
+                                    arr[index].text = e.target.value;
+                                    return arr;
                                 })
                             }}
                         />
                     </div>
                 ))
             }</main>
+           
+            <ReactTooltip id="my-tooltip" isOpen={toggleToolTp}/>
         </div>
     );
 };
--- src/customHook/todoHook.ts
+++ src/customHook/todoHook.ts
@@ -1,21 +1,53 @@
 import { useEffect, useState } from "react";
 
+export type ItemType = {
+    id:string;
+    text:string;
+}
 
 export function UseTodo(){
     const [status,setStatus] = useState('view');
-    const [item,setItem] = useState<string[] >([]);
+    const [item,setItem] = useState<ItemType[]>([]);
+    const [deleteList,setDeleteList] = useState<number[]>([]);
+    const [toggleToolTp,setToggleToolTp] = useState(false);
+    const [emptyInput,setEmptyInput] = useState<number | null>(null);
 
     useEffect(()=>{
-        console.log(item);
-    },[item])
-
+       console.log(item);
+    },[item]);
+    
+    // This add item together with random UUID and check if it has empty value
     const AddItem = () => {
-        if (item.includes(''))return;
-        setItem(prev=>[...prev,''])
-    }
-    const DeleteItem = () => {
+        const findIndex = item.findIndex((i,_)=> i.text === '');
+        setEmptyInput(findIndex === -1 ? null:findIndex);
         
+        const condition = item.some((item,_)=> item.text === '');
+        if(condition) {
+            setToggleToolTp(true);
+            setTimeout(()=>{
+                setToggleToolTp(false);
+            },1600);
+            return;
+        }else{
+            const value = {
+                id:crypto.randomUUID(),
+                text:''
+            }
+            setItem(prev=>[...prev,value]); 
+        }
     }
 
-    return {status,setStatus,item,setItem,AddItem,DeleteItem}
+    // This delete specific index from list
+    const DeleteItem = () => {
+        const indexSet = new Set(deleteList);
+        setItem(prev=>
+            prev.filter((_,index)=> !indexSet.has(index))
+        )
+        setDeleteList([]);
+    }
+
+    return {
+        status,setStatus,item,setItem,AddItem,DeleteItem,deleteList,setDeleteList,
+        toggleToolTp,emptyInput
+    }
 }
